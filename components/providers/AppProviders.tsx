93c9c328f54e7106a1a025925d9c696a26267aa0
Davina Wiha Sunjaya <153440351+nanaboo17@users.noreply.github.com>
2026-09-14 16:28:58 +0700

    Enable global auth session recovery

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from './theme-provider'
 import { I18nProvider } from './i18n-provider'
 import ImageBitmapFallback from './ImageBitmapFallback'
 import FieldFormDiagnostics from './FieldFormDiagnostics'
+import AuthSessionRecovery from './AuthSessionRecovery'
 import type { Locale } from '@/lib/i18n'
 import type { Theme } from '@/lib/theme'
 
@@ -20,6 +21,7 @@ export default function AppProviders({
   return (
     <ThemeProvider initialTheme={initialTheme}>
       <ImageBitmapFallback />
+      <AuthSessionRecovery />
       <FieldFormDiagnostics />
       <I18nProvider initialLocale={initialLocale}>{children}</I18nProvider>
     </ThemeProvider>
