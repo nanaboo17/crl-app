d533427537834a0df7ea923ed2eb773d550422a3
Davina Wiha Sunjaya <153440351+nanaboo17@users.noreply.github.com>
2026-09-14 15:57:46 +0700

    Enable field form diagnostics globally

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -4,6 +4,7 @@ import { type ReactNode } from 'react'
 import { ThemeProvider } from './theme-provider'
 import { I18nProvider } from './i18n-provider'
 import ImageBitmapFallback from './ImageBitmapFallback'
+import FieldFormDiagnostics from './FieldFormDiagnostics'
 import type { Locale } from '@/lib/i18n'
 import type { Theme } from '@/lib/theme'
 
@@ -19,6 +20,7 @@ export default function AppProviders({
   return (
     <ThemeProvider initialTheme={initialTheme}>
       <ImageBitmapFallback />
+      <FieldFormDiagnostics />
       <I18nProvider initialLocale={initialLocale}>{children}</I18nProvider>
     </ThemeProvider>
   )
